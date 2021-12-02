9a06fb465dc8028aac1f63be7b8085bbd2b4b01c
Zeedan345 <36732475+Zeedan345@users.noreply.github.com>
2021-12-02 12:16:02 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -5,12 +5,32 @@ import os
 import server
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="-")
 TOKEN = os.getenv("DISCORD_TOKEN")
+cogs = ['Basic','music']
+client.lava_nodes = [
+  {
+    'host': 'lava.link',
+    'port': 80,
+    'rest_uri' : f'http://lava.link:80',
+    'identifier':'MAIN',
+    'password':'anything',
+    'region': 'singapore'
+  }
+]
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
+    client.load_extension('dismusic')
+    print('Loading Cogs.....')
+    for cog in cogs:
+        try:
+            client.load_extension(cog)
+            print(cog + " was Loaded")
+
+        except Exception as e:
+            print(e) 
 
 @bot.command()
 async def ping(ctx):
